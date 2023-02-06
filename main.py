1861e8d983227e87461faf12c3e4aded6878025e
Joel Vernon <joel.l.vernon@wmich.edu>
2023-02-06 00:47:20 -0500

    Switched to OOP approach

--- main.py
+++ main.py
@@ -1,29 +1,31 @@
-JAR_SIZES = [70, 50, 20]
+class MinimumJarCalculator:
+    def __init__(self, milk_amount):
+        self.milk_amount = milk_amount
+        self.solutions = []
+        self.JAR_SIZES = [70, 50, 20]
 
-
-def calculate_minimum_jars(milk_amount: int):
-    def backtrack(milk_amount, jars=0):
-
+    def backtrack(self, milk_amount, jars=0):
         if milk_amount < 0:
             return
 
         if milk_amount == 0:
-            solutions.append(jars)
+            self.solutions.append(jars)
+            return
 
-        for jar_size in JAR_SIZES:
-            backtrack(milk_amount - jar_size, jars + 1)
+        for jar_size in self.JAR_SIZES:
+            self.backtrack(milk_amount - jar_size, jars + 1)
 
-    solutions = []
-    backtrack(milk_amount)
-
-    try:
-        return f"Minimum number of jars used is {min(solutions)}"
-    except ValueError:
-        return f"Sorry !!! Cannot measure the required volume"
+    def find_minimum_jar_count(self):
+        self.backtrack(self.milk_amount)
+        try:
+            return print(f"Minimum number of jars used is: {min(self.solutions)}")
+        except ValueError:
+            return print("Sorry !!! Cannot measure required volume")
 
 
 def main():
-    print(calculate_minimum_jars(0))
+    minimum_jar_calculator = MinimumJarCalculator(140)
+    minimum_jar_calculator.find_minimum_jar_count()
 
 
 if __name__ == "__main__":
