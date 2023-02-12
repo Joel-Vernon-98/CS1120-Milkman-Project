28ef51fd57377d4c0325a9281daa581aaa3bf5ce
Joel Vernon <joel.l.vernon@wmich.edu>
2023-02-12 02:31:05 -0600

    Better implementation of Branch and bound

--- main.py
+++ main.py
@@ -1,30 +1,35 @@
 JAR_SIZES = [70, 50, 20]
+from time import perf_counter_ns
 
 
-def calculate_minimum_jars(milk_amount: int):
-    def backtrack(milk_amount, jars=0):
-
-        if milk_amount < 0:
+def calculate_minimum_required_jars(milk_amount: int):
+    def backtrack(volume, jars=0):
+        if volume in seen:
             return
-
-        if milk_amount == 0:
-            solutions.append(jars)
-
+        if volume < 0:
+            seen.add(volume)
+            return
+        if jars < min(solutions, default=0):
+            return
+        if volume == 0:
+            solutions.add(jars)
+            return
         for jar_size in JAR_SIZES:
-            backtrack(milk_amount - jar_size, jars + 1)
+            seen.add(volume)
+            backtrack(volume - jar_size, jars + 1)
 
-    solutions = []
+    seen = set()
+    solutions = set()
     backtrack(milk_amount)
 
-    try:
-        return f"Minimum number of jars used is {min(solutions)}"
-    except ValueError:
-        return f"Sorry !!! Cannot measure the required volume"
-
+    return min(solutions, default="None")
 
 def main():
-    print(calculate_minimum_jars(550))
+    print(calculate_minimum_required_jars(600))
 
 
 if __name__ == "__main__":
+    start = perf_counter_ns()
     main()
+    end = perf_counter_ns()
+    print(end - start)
