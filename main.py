009c6f2c952ffaef02897c9e6b1597ee8315deef
Joel Vernon <joel.l.vernon@wmich.edu>
2023-02-12 15:19:07 -0600

    misc improvements

--- main.py
+++ main.py
@@ -1,31 +1,39 @@
-JAR_SIZES = [70, 50, 20]
+JAR_SIZES = (70, 50, 20)
 from time import perf_counter_ns
 
 
 def calculate_minimum_required_jars(milk_amount: int):
-    def backtrack(volume, jars=0):
-        if volume in seen:
+    def backtrack(volume, solution=0, jars=0):
+
+        if jars < min(solutions, default=-1):
             return
-        if volume < 0:
-            seen.add(volume)
-            return
-        if jars < min(solutions, default=0):
-            return
+
         if volume == 0:
             solutions.add(jars)
             return
+
         for jar_size in JAR_SIZES:
-            seen.add(volume)
-            backtrack(volume - jar_size, jars + 1)
 
-    seen = set()
+            next_jar = volume - jar_size
+            if next_jar >= 20 or next_jar == 0:
+                backtrack(volume - jar_size, solution, jars + 1)
+            else:
+                continue
+
     solutions = set()
-    backtrack(milk_amount)
+    if milk_amount % 10 != 0:
+        return print("Sorry !!! Cannot measure the required volume")
 
-    return min(solutions, default="None")
+    backtrack(milk_amount)
+    try:
+        print(solutions, sep="\n")
+        return print(f"Minimum number of jars used: {min(solutions)}")
+    except ValueError:
+        return print("Sorry !!! Cannot measure the required volume")
+
 
 def main():
-    print(calculate_minimum_required_jars(600))
+    calculate_minimum_required_jars(65000)
 
 
 if __name__ == "__main__":
