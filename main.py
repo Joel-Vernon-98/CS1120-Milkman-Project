f5f01ec285e375f7f568b7958e4c0baa26c8b54d
= <=>
2023-02-14 19:42:19 -0500

    finalized code to turn in

--- main.py
+++ main.py
@@ -1,42 +1,44 @@
-
-from time import perf_counter
 import sys
-
 JAR_SIZES = (70, 50, 20)
 
 
 def calc_minimum_jars():
-    milk_amount = 69440
-    extended = set()
 
-    def branch_and_bound(volume, min_req_jars=0, solution=sys.maxsize):
-
+    def dfs(volume, min_req_jars=0, solution=sys.maxsize):
         extended.add(volume)
-
         if volume == 0:
             return min(solution, min_req_jars)
-
         for jar in JAR_SIZES:
-
             next_volume = volume - jar
             next_min_req_jars = min_req_jars + 1
-
             if next_min_req_jars >= solution:
                 return solution
-
             if next_volume in extended:
                 return solution
-
             if next_volume >= 20 or next_volume == 0:
-                solution = branch_and_bound(next_volume, next_min_req_jars, solution)
-
+                solution = dfs(next_volume, next_min_req_jars, solution)
         return solution
 
-    if milk_amount % 10 != 0:
-        return "No Results Found"
+    valid_input = False
+    while not valid_input:
+        try:
+            milk_amount = int(input("Enter milk amount: "))
+            if milk_amount > 69440:
+                raise RecursionError
+            valid_input = True
+        except ValueError:
+            print("Invalid Input: Please Enter an integer.")
 
-    results = branch_and_bound(milk_amount)
-    return results if results != sys.maxsize else "No Results Found"
+        except RecursionError:
+            print("Volumes larger than 69440ml are too large to calculate, please enter a smaller number.")
+
+    extended = set()
+
+    if milk_amount % 10 != 0:
+        return "Sorry !!! Cannot measure the required volume"
+    results = dfs(milk_amount)
+    return f"Minimum number of jars required is: {results}" if results != sys.maxsize \
+        else "Sorry !!! Cannot measure the required volume"
 
 
 def main():
@@ -44,7 +46,5 @@ def main():
 
 
 if __name__ == "__main__":
-    start = perf_counter()
     main()
-    end = perf_counter()
-    print(end-start)
+
